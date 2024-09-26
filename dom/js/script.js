e7f18306f561ef3b3d95641c7a465458b9b0a6f2
myllennah <milena.rmota@senacsp.edu.br>
2024-09-26 08:44:06 -0300

    add 'objetos' and 'dom'

---
+++ dom/js/script.js
@@ -0,0 +1,10 @@
+//DOM movimentando-se no console
+console.log(document.body);
+
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1].childNodes);
+
+//propriedade para acessar o elemento do título
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+console.log(document.body.childNodes[5].childNodes[1].textContent);
